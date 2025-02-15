2df84437e5d38be780fc2cb0b56ea02717171180
markodvt <markodvt@gmail.com>
2025-02-15 15:32:24 -0500

    Working Zombies, but without shooting arrows

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class Zombie:
         self.alive = True
 
     def __repr__(self):
-        return f'Zombie: name:{self.name}, distance: {self.distance}, health: {self.health}, rounds: {self.rounds}, alive: {self.alive}'
+        return f'Zombie: name: {self.name}, distance: {self.distance}, speed: {self.speed}, health: {self.health}, rounds: {self.rounds}, alive: {self.alive}'
     
     def move(self):
         '''Move Zombie's position to max(0, self.distance - self.speed), returning Zombie's new distance to player.
@@ -34,13 +34,17 @@ class Zombie:
 
     @classmethod
     def TESTME(self):
+        '''Creates some Zombies. At each round, the zombies move. Ends when a zombie's distance reaches 0.
+        No arrows yet, so zombies just keep advancing!! We expect Chuck to win the race given his initial distance (20) and speed (8).
+        Round 1: Chuck starts d=20, ends d=12; Round 2: Chuck d=12 drops to d=4; Round 3: Chuck reaches d=0; Game stops.
+        '''
         game_round = 1
-        zombies = [Zombie("Abe", 10, 1, 5), Zombie("Bill", 200, 40, 20), Zombie("Chuck", 20, 5, 10)]
+        zombies = [Zombie("Abe", 10, 1, 5), Zombie("Bill", 200, 40, 20), Zombie("Chuck", 20, 8, 10)]
         for zombie in zombies:
-            print(f'Zombie created: {zombie.name}')
+            print(f'Zombie created: {zombie}')
         min_distance = min(z.distance for z in zombies)
         while min_distance > 0:
-            print(f'Round: {game_round} .................')
+            print(f'Round: {game_round} ........................................')
             print(f'State at start ............')
             for z in zombies:
                 print(z) 
@@ -49,7 +53,8 @@ class Zombie:
             print(f'State at end ..........')   
             for z in zombies:
                 print(z) 
-            game_round += 1         
+            game_round += 1
+            min_distance = min(z.distance for z in zombies)
  
 # Start by tracking game as global variables. Later probably a good idea to create a Game class
 
