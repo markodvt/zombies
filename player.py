e7e47250a994d4696823e6af1e924ec95f63e881
markodvt <markodvt@gmail.com>
2025-02-17 09:25:24 -0500

    simplify Player by moving priority queue into Game

--- player.py
+++ player.py
@@ -1,11 +1,8 @@
+from priority_queue import Priority_queue
+
+
 class Player:
-    '''Player has a name and arrow_capacity. 
-
-    TODO - implement strategy later so player can prioritize their arrows.
-    Prioritize shooting the zombie with the lowest ETA. You may shoot the same zombie with several arrows during a round, but do not continue to shoot a zombie that has been destroyed (i.e. after its health has reached zero).
-
-    In the event of ties in ETA, you should shoot the zombie with the lower health.
-    If zombies are also tied in health, you should shoot the zombie with the lexicographically smaller name.
+    '''Player has a name and arrow_capacity. At each round, player reloads arrows.
     '''
     def __init__(self, name, quiver_capacity, zombie_queue=None, alive=True, entered_in_round = 1, killed_in_round = None, killed_by = None):
         self.name = name
@@ -15,7 +12,6 @@ class Player:
         self.killed_in_round = killed_in_round
         self.killed_by = killed_by
         self.remaining_arrows = 0
-        self.zombie_queue = zombie_queue
         
     def __repr__(self):
         # return self.__class__.__name__ + str(self.__dict__)
@@ -33,16 +29,18 @@ class Player:
             self.killed_by = zombie
             self.killed_in_round = round
 
+    '''
     def update_zombie_queue(self, zombie_array):
-        '''Generate a priority queue based on updated attributes of the zombies in the zombie_array. This is used in a game, after the array game.zombies reflects new positions of the zombies.
+        Generate a priority queue based on updated attributes of the zombies in the zombie_array. This is used in a game, after the array game.zombies reflects new positions of the zombies.
         
         Begin with naiive implementation ... just return 0..len(array)
-        '''
+        
         self.zombie_gueue = list(range(len(zombie_array)))
 
     def next_target_zombie(self):
-        '''Returns index (in zombie_array) of the most dangerous zombie, so player can aim available arrows at zombie_array[next_target_zombie()].
+        Returns index (in zombie_array) of the most dangerous zombie, so player can aim available arrows at zombie_array[next_target_zombie()].
         
         Begin with naiive implementation ... just return next index.
-        '''
+        
         return self.zombie_gueue.pop(0)
+    '''
