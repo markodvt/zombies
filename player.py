fd9f6a8f6fabb757417f718c0ffb6e45aa7ca2fd
markodvt <markodvt@gmail.com>
2025-02-17 10:07:00 -0500

    add Player TestMe

--- player.py
+++ player.py
@@ -1,10 +1,11 @@
 from priority_queue import Priority_queue
+from zombie import Zombie
 
 
 class Player:
     '''Player has a name and arrow_capacity. At each round, player reloads arrows.
     '''
-    def __init__(self, name, quiver_capacity, zombie_queue=None, alive=True, entered_in_round = 1, killed_in_round = None, killed_by = None):
+    def __init__(self, name, quiver_capacity, alive=True, entered_in_round = 1, killed_in_round = None, killed_by = None):
         self.name = name
         self.quiver_capacity = quiver_capacity
         self.alive = True
@@ -28,3 +29,24 @@ class Player:
             self.alive = False
             self.killed_by = zombie
             self.killed_in_round = round
+
+    @classmethod
+    def TestMe(cls):
+        print(f'\n{'='*40}\nTesting class: {cls.__name__}\n{'='*40}')
+
+        player1 = Player('Steve', 20)
+        player2 = Player('Jimmy', 50)
+        zombie = Zombie('Zack', 0, 20, 10)
+
+        print(f'Players ........\n{player1}\n{player2}')
+        print(f'\nUpdate:  Player 1 gets killed by a zombie named Zack in Round 6\n')
+        player1.killed(zombie, 6)
+        print(f'Players ........\n{player1}\n{player2}')
+        
+        
+def main():
+    Player.TestMe()
+
+if __name__ == '__main__':
+    main()
+
